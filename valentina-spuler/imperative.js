a664eb2e0703325cb95a27712eecd426bf875016
relupselav <vmspuler@gmail.com>
2017-08-08 12:03:42 -0400

    Añadido break para evitar loop infinito

--- valentina-spuler/imperative.js
+++ valentina-spuler/imperative.js
@@ -13,9 +13,10 @@ for (var i = 2; i < 20; i++) {
 
   // TU CÓDIGO AQUÍ
 
-  for (var j = 2; 2 <= i/2; j++){
+  for (var j = 2; j <= (i/2); j++){
     if (i % j === 0) {
 			isPrime = false;
+      break;
     }
   }
 
